79ff93c6c01d925fa2fff4ecf1419a6a5647927d
Axcol3 <asirovaalina287@gmail.com>
2025-04-13 10:36:19 +0600

    Delete TrackingHealth directory

--- TrackingHealth/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-import org.example.lib.Labels.MainLabel;
-
-public class Main {
-    public static void main(String[] args) {
-        // Create an instance of the MainLabel class to initialize and show the login screen
-        MainLabel main = new MainLabel();
-    }
-}
